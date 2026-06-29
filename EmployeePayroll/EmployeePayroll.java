9127eef7b769b2b1f2b7c74b328bb8bd80a8a573
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-06-29 11:36:15 +0530

    [Gayatri:Welcome to EmployeePayroll problem]

---
+++ EmployeePayroll/EmployeePayroll.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz.EmployeePayroll;
+
+public class EmployeePayroll {
+    public static void main(String[] args) {
+        System.out.print("Welocme to employee payroll project");
+    }
+}
